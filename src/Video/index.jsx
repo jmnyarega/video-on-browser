8e098a5e76c2a66ba7a0143b715ed89add73acc1
josiah <jmnyarega@gmail.com>
2020-10-26 01:37:28 +0300

    compliant

--- src/Video/index.jsx
+++ src/Video/index.jsx
@@ -21,7 +21,7 @@ class App extends React.Component {
     for (let file of xfiles) {
       files = files.concat({
         name: file.name,
-        url: `http://127.0.0.1:8080/${file.name}`,
+        url: `https://127.0.0.1:8080/${file.name}`,
         size: file.size,
         type: file.type,
       });
